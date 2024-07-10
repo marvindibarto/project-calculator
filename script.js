6cae8549958e060f59b55ee89dc981129bca0e60
marvindibarto <marvin.dibartolomeo@gmail.com>
2024-07-10 19:46:34 +0200

    created function called 'operate' to do calculations based on inputs

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+
+function operate(a,b,operator){
+    switch (operator) {
+        case "*":
+            const result = multiply(a,b)
+            break;
+        case "/":
+            const result = divide(a,b)
+            break;
+        case "+":
+            const result = add(a,b)
+            break;
+        case "-":
+            const result = subtract(a,b)
+            break;
+    }
+    return result;
+}
+
 function add(a, b){
     return a+b;
 }
